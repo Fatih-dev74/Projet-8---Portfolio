301ad47b559b280b2102cad0af5457caff5c48b5
Fatih-dev74 <fatihcigdem74100@gmail.com>
2024-11-25 09:09:32 +0100

    -

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/Projet8-Portfolio/', // Remplace par le nom de ton dépôt
+});
